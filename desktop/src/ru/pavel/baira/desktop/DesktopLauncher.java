d5e2e0a72d69ff724c4675fa814bca29920cf569
obairka <obairka@gmail.com>
2017-10-29 21:36:49 +0700

    initial commit

---
+++ desktop/src/ru/pavel/baira/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.pavel.baira.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.pavel.baira.TodockGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TodockGame(), config);
+	}
+}
